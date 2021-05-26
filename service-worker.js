9a53918b2d0ff8547cdbaf268aabb174b7ac42e3
Olga Tsarova <alita.jet@gmail.com>
2021-05-27 01:29:47 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.520d4294590a353bae9dfdf7775894f7.js"
+  "./precache-manifest.e89a2f61bfee7e0e4f6db9be48303c1f.js"
 );
 
 self.addEventListener('message', (event) => {
